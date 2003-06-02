82e33302381b6164aff5bb0a9acff47a623e350a
Zhuohuan LI <zixia@zixia.net>
2003-06-02 14:50:21 +0000

    增加个人网上支付系统的英文版

---
+++ pay/p/epay.php
@@ -0,0 +1,42 @@
+<table width="760" border="0" cellspacing="0" cellpadding="0">
+  <tr> 
+    <td width="550" height="224" valign="top"> 
+<br>
+      <center>
+      </center>
+<table width="500" border="0" cellspacing="0" cellpadding="0">
+  <tr> 
+  <td align="center" >
+<?
+if ( (!isset($_REQUEST['u']) ) ) {
+?>
+Please specify user ID<br>
+<?
+}else {
+?>
+<br><br><br>
+
+<center>
+<form action="eprepare.php" name=form method=post>
+Please input the amount:<p>
+$<input type=text name=MoneyCount size=4><br><p>
+<input type=hidden name=MoneyType value=1>
+<input type=hidden name=UserID value=<?=$_REQUEST['u']?>>
+
+<input type=submit name=submit value='Go to pay it!'>
+</form>
+</center>
+<br><br><br>
+
+<?
+}
+?>
+</td></tr></table>
+<br>
+
+
+    </td>
+    <td width="210" height="224" bgcolor="F0FAFF" valign="top">&nbsp;
+</td>
+  </tr>
+</table>
